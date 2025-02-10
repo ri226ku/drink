392fb7793aabacb3cc9dd5410a5b59d932cab497
ri226ku <ri218718u@icloud.com>
2025-02-11 02:53:01 +0900

    bonus-drinkの回答を提出

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php 
+fscanf(STDIN, "%d", $buy);
+$present = 0;
+
+$flg=0;
+if($buy >= 3){
+  $drank =$buy;
+  $flg = 1;
+  while($flg == 1){
+    $get = floor($drank / 3);
+    $present = $present + $get;
+    $leave = $drank % 3;
+    $drank = $get +$leave;
+    if($drank < 3){
+      $flg =0;
+    }
+  }
+}
+echo $buy + $present.PHP_EOL;
+
+?>
